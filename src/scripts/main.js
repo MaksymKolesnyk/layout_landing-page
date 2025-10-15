7e212f211c125e503c82be239f514a561e26791e
MaksymKolesnyk <maksymka@icloud.com>
2025-10-14 22:36:49 -0400

    Landing page v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+/*We change anchor for phone icon can close and open menu with 
+ contacts to call only on Desktop  */
+
+const phoneIcon = document.querySelector('.icon--phone');
+
+phoneIcon.addEventListener('click', () => {
+  const value = phoneIcon.getAttribute('href');
+
+  if (value === '#contact-top') {
+    phoneIcon.setAttribute('href', '#');
+  } else {
+    phoneIcon.setAttribute('href', '#contact-top');
+  }
+});
